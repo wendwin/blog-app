f0df11874d798e18eb37c8b6601faffd9b621590
wendwin <wendwin12@gmail.com>
2025-01-23 14:44:47 +0700

    menambahkan fitur komentar, membuat model, model form dan views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 # def post_list(request):
 #     post_list = Post.published.all()
@@ -67,4 +68,21 @@ def post_share(request, post_id):
         'post': post,
         'form': form,
         'sent': sent
+    })
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    
+    return render(request, 'blog/post/comment.html',{
+        'post': post,
+        'form': form,
+        'comment': comment
     })

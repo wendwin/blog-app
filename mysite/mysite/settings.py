97db2cb3f7546a99707d13a99aafba7570fb920d
wendwin <wendwin12@gmail.com>
2025-02-10 21:11:48 +0700

    menambahkan fitur custom template filter, sitemap dan feed blog

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django.contrib.staticfiles',
     'taggit',
     'blog.apps.BlogConfig',
